5cc0f37ee51e2831d2a9fe30553de93f5124d788
Eurvin <eurvin@duck.com>
2025-11-18 12:27:23 +0100

    added basic setup
    - setup window size
    - coordinate system
    - set background color to 'BLACK'

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
 use nannou::prelude::*;
 
+const ROWS: u32 = 32;
+const COLS: u32 = 20;
+const SIZE: u32 = 26;
+const HEIGHT: u32 = ROWS * SIZE;
+const WIDTH: u32 = COLS * SIZE;
+const LINE_WIDTH: f32 = 0.03;
+
 fn main() {
-    nannou::app(model)
-        .update(update)
-        .loop_mode(LoopMode::loop_once())
-        .run()
+    nannou::app(model).update(update).run();
 }
 
-struct Model {}
+struct Model {
+    _window: window::Id,
+}
 
-fn model(_app: &App) {}
+fn model(app: &App) -> Model {
+    let _window = app
+        .new_window()
+        .title(app.exe_name().unwrap())
+        .size(WIDTH, HEIGHT)
+        .view(view)
+        .build()
+        .unwrap();
 
-fn view(app: &App, _model: &Model, _frame: Frame) {}
+    Model { _window }
+}
 
-fn update(_app: &App, model: &Model, _update: Update) {}
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let draw = app.draw();
+    draw.scale(SIZE as f32).x_y(ROWS as f32, COLS as f32);
+
+    draw.background().color(BLACK);
+    draw.to_frame(app, &frame).unwrap();
+}
