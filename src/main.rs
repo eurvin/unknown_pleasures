3a769806144616b361482306ee17e0c4522d6e57
Eurvin <eurvin@duck.com>
2025-11-18 11:22:36 +0100

    inital setup
    - added nannou and midir to Cargo.toml
    - added basic to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use nannou::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    nannou::app(model)
+        .update(update)
+        .loop_mode(LoopMode::loop_once())
+        .run()
 }
+
+struct Model {}
+
+fn model(_app: &App) {}
+
+fn view(app: &App, _model: &Model, _frame: Frame) {}
+
+fn update(_app: &App, model: &Model, _update: Update) {}
