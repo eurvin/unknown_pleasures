84000e0aa1f2038171686fcda9ba6a309b682e21
Eurvin <eurvin@duck.com>
2025-11-19 12:14:29 +0100

    added first straight line version
    - added loopMode to run a single loop
    - added Lin struct and implementation
    - added vector of lines to model
    - added random seed to model for future seeded randomness
    - updated update function - no content as of yet

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
+use nannou::noise::*;
 use nannou::prelude::*;
 
-const ROWS: u32 = 32;
-const COLS: u32 = 20;
-const SIZE: u32 = 26;
-const HEIGHT: u32 = ROWS * SIZE;
-const WIDTH: u32 = COLS * SIZE;
-const LINE_WIDTH: f32 = 0.03;
+const ROWS: u32 = 50;
+const SIZE: u32 = 15;
+const MARGIN: u32 = 50;
+const HEIGHT: u32 = 800;
+const WIDTH: u32 = 600;
+const LINE_WIDTH: f32 = 0.04;
 
 fn main() {
-    nannou::app(model).update(update).run();
+    nannou::app(model)
+        .update(update)
+        .loop_mode(LoopMode::loop_once())
+        .run();
 }
 
+#[derive(Debug)]
+struct Line {
+    start: Vec2,
+    end: Vec2,
+}
+
+impl Line {
+    fn new(y: f32) -> Self {
+        let start = pt2(-1.0, y);
+        let end = pt2(1.0, y);
+        Line { start, end }
+    }
+}
+
+#[derive(Debug)]
 struct Model {
-    _window: window::Id,
+    lines: Vec<Line>,
+    random_seed: u32,
 }
 
 fn model(app: &App) -> Model {
@@ -24,15 +44,43 @@ fn model(app: &App) -> Model {
         .build()
         .unwrap();
 
-    Model { _window }
+    let mut lines = Vec::new();
+
+    for y in 0..ROWS {
+        let line = Line::new(y as f32);
+        println!("{:?}", line);
+        lines.push(line);
+    }
+
+    Model {
+        lines,
+        random_seed: 43,
+    }
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    let draw = app.draw();
+    let noise = Perlin::new();
+
+    let gdraw = draw
+        .scale_y(SIZE as f32)
+        .scale_x(WIDTH as f32 / 2.0 - MARGIN as f32 * 2.0)
+        .scale_y(-1.0)
+        .y(ROWS as f32 / -2.0 + 0.5);
+
+    draw.background().color(BLACK);
+
+    for line in &model.lines {
+        let ldraw = &gdraw;
+        ldraw
+            .line()
+            .start(line.start)
+            .end(line.end)
+            .weight(LINE_WIDTH)
+            .color(WHITE);
+    }
+
+    draw.to_frame(app, &frame).unwrap();
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
-
-fn view(app: &App, _model: &Model, frame: Frame) {
-    let draw = app.draw();
-    draw.scale(SIZE as f32).x_y(ROWS as f32, COLS as f32);
-
-    draw.background().color(BLACK);
-    draw.to_frame(app, &frame).unwrap();
-}
